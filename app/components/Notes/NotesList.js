93b75e1f6389980c5e660c4ffaa9d5ce510f160c
Mateo Clarke <code@mateoclarke.com>
2015-10-26 22:08:38 -0500

    vid 13: ES6 Classes and PropTypes

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -1,8 +1,8 @@
-var React = require('react');
+import React from 'react';
 
-var NotesList = React.createClass({
-  render: function(){
-    var notes = this.props.notes.map(function(note, index){
+class NotesList extends React.Component{
+  render(){
+    var notes = this.props.notes.map((note, index) => {
       return (
         <li className="list-group-item" key={index}>
           {note}
@@ -13,6 +13,6 @@ var NotesList = React.createClass({
       <ul className="list-group">{notes}</ul>
     )
   }
-});
+};
 
-module.exports = NotesList;
+export default NotesList;
